e4cab944360a48504f8c652ecda4801f1bfda8f6
Entue250 <edouardoniyomugabo@gmail.com>
2026-06-09 17:24:32 +0200

    Add Widget test

---
+++ test/widget_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('placeholder test', (WidgetTester tester) async {
+    // Tests will be added when UI screens are built
+    expect(true, true);
+  });
+}
